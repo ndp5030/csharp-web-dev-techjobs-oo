301fc78d7bcae877c951c7bbe23ccb145a62161a
Nicholas Pfleegor <ndp5030@gmail.com>
2022-05-12 22:24:46 -0400

    Custom overrides set in all classes and constructors added

--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -28,6 +28,16 @@ namespace TechJobsOO
             JobCoreCompetency = jobCoreCompetency;
         }
         // TODO: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
 
     }
 }
